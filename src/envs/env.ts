a0d8f0bb347b4b0a29ddf0a84b8358fe125b4abb
Jaweria Siddiqui <jaweria.siddiqui@startrum.com>
2022-03-14 11:17:50 +0500

    added android.yml file

--- src/envs/env.ts
+++ src/envs/env.ts
@@ -1,5 +1,5 @@
 const Env = {
-  CURRENT: 'DEV',
+  CURRENT: "DEV",
   BASE_URL: "https://thebarcode-dev.cygnis.dev/",
   SHOULD_ENABLE_FORCE_UPDATE: false,
   ONESIGNAL_ID: "5ce0f111-23bc-4aec-bc4e-b11bf065cfc8",
